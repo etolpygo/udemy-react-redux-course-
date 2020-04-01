cffeffe828c0b97529e8e63d3e514784e8fea957
elena.cranley <elena.cranley@ucop.edu>
2020-04-01 02:27:20 -0700

    add birthday state; pass on to new AgeStats component; calculate num of years month days

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 import '../styles/App.css';
+import AgeStats from './AgeStats';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+      birthday: '1980-04-04'
+    }
+  }
+
+  changeBirthday() {
+    this.setState({
+      birthday: this.state.newDate
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
         <Form inline>
           <h3>Input Your Birthday:</h3>
-          <FormControl type="date">
+          <FormControl type="date" onChange={ event => this.setState({ newDate: event.target.value })}>
           </FormControl>
           {' '}
-          <Button>
+          <Button onClick={ () => this.changeBirthday()}>
             Submit
           </Button>
         </Form>
+        <AgeStats date={this.state.birthday} />
       </div>
     );
   }
