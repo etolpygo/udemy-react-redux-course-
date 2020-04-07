26674269bac8f45b5845e4be21473ed5ba2589ec
elena.cranley <elena.cranley@ucop.edu>
2020-04-06 17:41:41 -0700

    add first reducer

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/App';
-import rootReducer from ./reducers';
+import rootReducer from './reducers';
+import { addCharacterById } from './actions';
 
 const store = createStore(rootReducer);
+console.log('store.getState()', store.getState());
+
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
   <Provider store={store}>
