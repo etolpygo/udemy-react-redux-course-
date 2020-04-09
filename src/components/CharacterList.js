f39e4e2c7c40e719553f1c5d062a12b6ee0422a4
elena.cranley <elena.cranley@ucop.edu>
2020-04-08 19:03:55 -0700

    import addCharacterById from actions into CharacterList using mapDispatchToProps

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addCharacterById } from '../actions';
 
 class CharacterList extends Component {
   render() {
+    console.log('this.props', this.props);
     return(
       <div>
         <h4>Characters</h4>
@@ -13,6 +16,8 @@ class CharacterList extends Component {
                 return(
                   <li key={character.id}>
                     <div>{character.name}</div>
+                    <div onClick={() => this.props.addCharacterById(character.id)}
+                    >+</div>
                   </li>
                 )
               }
@@ -30,4 +35,8 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, null)(CharacterList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ addCharacterById }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
