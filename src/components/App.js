12f0013f8f7204326ecc2709ae6b3fabcca13b5a
elena.cranley <elena.cranley@ucop.edu>
2020-04-09 21:20:39 -0700

    basic SearchRecipes component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/index.css'
+import SearchRecipes from './SearchRecipes';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Recipe Finder</h2>
+        <SearchRecipes />
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/SearchRecipes.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
+
+class SearchRecipes extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ingredients: '',
+      dish: ''
+    }
+
+  }
+
+  search() {
+    let { ingredients, dish } = this.state;
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+    console.log('this.state', this.state);
+    console.log(url);
+
+  }
+
+  render() {
+    return (
+      <Form inline>
+        <FormGroup>
+            <FormLabel>Ingredients</FormLabel>
+            {' '}
+            <FormControl
+              type="text" placeholder="tofu, garlic" onChange={event => this.setState({ ingredients: event.target.value })} />
+            </FormGroup>
+        {' '}
+        <FormGroup>
+            <FormLabel>Dish</FormLabel>
+            {' '}
+            <FormControl
+              type="text" placeholder="adobo" onChange={event => this.setState({ dish: event.target.value })} />
+            </FormGroup>
+            <Button onClick={() => this.search()}>Submit</Button>
+      </Form>
+    )
+  }
+}
+
+export default SearchRecipes;
