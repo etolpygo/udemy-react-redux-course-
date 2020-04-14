179e4bb375d472005d63427c08c33322333cb762
elena.cranley <elena.cranley@ucop.edu>
2020-04-13 23:23:17 -0700

    display list of memes up to memeLimit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../styles/index.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      memeLimit: 10
+    }
+
+  }
   render() {
     return (
       <div>
         <h2>Welcome to the Meme Generator</h2>
+        {
+          this.props.memes.slice(0, this.state.memeLimit).map((meme) => {
+            return(
+              <h4 key={meme.id}>{meme.name}</h4>
+            )
+          })
+        }
+        <div className="loadMemesLink" onClick={() => this.setState({ memeLimit: this.state.memeLimit + 10 })}>Load 10 more memes...</div>
       </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return state;
+}
+
+export default connect(mapStateToProps, null)(App);
