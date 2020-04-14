142fe8ed9fe60f44a9b9a51c93553032d6ab6320
elena.cranley <elena.cranley@ucop.edu>
2020-04-14 12:54:30 -0700

    form for top and bottom meme text; pass params into MemeItem

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../styles/index.css';
+import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
+
 import MemeItem from './MemeItem';
 
 class App extends Component {
@@ -8,18 +10,37 @@ class App extends Component {
     super();
 
     this.state = {
-      memeLimit: 10
+      memeLimit: 10,
+      text0: '',
+      text1: ''
     }
 
   }
   render() {
     return (
       <div>
-        <h2>Welcome to the Meme Generator</h2>
+        <h2><u>Welcome to the Meme Generator</u></h2>
+        <h4><i>Write Some Text</i></h4>
+        <Form inline>
+          <FormGroup>
+            <FormLabel>Top</FormLabel>
+            {' '}
+            <FormControl type="text" onChange={event => this.setState({text0: event.target.value})}></FormControl>
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>Bottom</FormLabel>
+            {' '}
+            <FormControl type="text" onChange={event => this.setState({text1: event.target.value})}></FormControl>
+          </FormGroup>
+        </Form>
         {
           this.props.memes.slice(0, this.state.memeLimit).map((meme) => {
             return(
-              <MemeItem key={meme.id} meme={meme} />
+              <MemeItem
+                key={meme.id}
+                meme={meme}
+                text0={this.state.text0}
+                text1={this.state.text1} />
             )
           })
         }
