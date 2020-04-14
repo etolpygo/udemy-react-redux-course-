9c65cf48689d3b9cb149d79fb8938ea0c3518d27
elena.cranley <elena.cranley@ucop.edu>
2020-04-14 01:30:20 -0700

    hover and unhover styles for MemeItem

--- src/components/MemeItem.js
+++ src/components/MemeItem.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
 class MemeItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hovered: false
+    }
+  }
+
   render() {
     let { meme } = this.props;
     return(
-      <div className="meme-item">
-        <img className="meme-img"
+      <div
+        className="meme-item"
+        onMouseEnter={() => this.setState({ hovered: true })}
+        onMouseLeave={() => this.setState({ hovered: false })}>
+        <img className={ this.state.hovered ? "meme-img darken-img" : "meme-img" }
           src={meme.url}
           alt={meme.name}
           />
-        <p className="meme-txt">{meme.name}</p>
+        <p className={ this.state.hovered ? "meme-txt" : "no-txt" }>{meme.name}</p>
       </div>
     );
   }
